4180427def5fc428f9d118537dc39cd1efacbd76
Jacques Le Normand <jacqueslenormand@gmail.com>
2019-11-10 12:52:31 -0500

    first commit

---
+++ src/SearchResults.jsx
@@ -0,0 +1,22 @@
+import { connect } from 'react-redux'
+import React, { Component } from 'react'
+import data from './data.js'
+class UnconnectedSearchResults extends Component {
+    render = () => {
+        let results = data.filter(item => {
+            return item.name.includes(this.props.query)
+        })
+        return (<div>
+            {results.map(r => {
+                return (<div>{r.name}</div>)
+            })}
+        </div>)
+    }
+}
+let mapStateToProps = st => {
+    return {
+        query: st.searchQuery,
+    }
+}
+let SearchResults = connect(mapStateToProps)(UnconnectedSearchResults)
+export default SearchResults  
